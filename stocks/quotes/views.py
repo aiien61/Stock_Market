a75cf05b8f0a326047641a9257981a0de011b105
aiien61 <aiien61will@gmail.com>
2023-08-31 03:22:44 +0800

    create api request

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -1,8 +1,25 @@
+import os
+import json
+
+import requests
 from django.shortcuts import render
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def home(request):
-    return render(request, 'index.html', {})
+    parameters = {
+        'token': os.environ.get("API_KEY")
+    }
+    response = requests.get(url='https://api.iex.cloud/v1/data/core/quote/aapl', params=parameters)
+    try:
+        response.raise_for_status()
+        data = response.json()
+    except Exception as e:
+        data = "Error..."
+    
+    return render(request, 'index.html', {'data': data})
 
 
 def about(request):
