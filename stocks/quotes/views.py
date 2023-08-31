78a19d42754dcaa7e035b3f940a8e0cabd2c2fb6
aiien61 <aiien61will@gmail.com>
2023-09-01 01:54:12 +0800

    complete add form & delete button & delete page

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -11,18 +11,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 ENDPOINT = 'https://api.iex.cloud/v1/data/core/quote'
+parameters = {'token': os.environ.get("API_KEY")}
 
 
 def home(request):
     if request.method == "POST":
         try:
             symbol = request.POST["ticker"]
-            parameters = {
-                'token': os.environ.get("API_KEY")
-            }
             target_url = os.path.join(ENDPOINT, symbol)
             response = requests.get(url=target_url, params=parameters)
-            
             response.raise_for_status()
             
             # response structure often changes e.g. list or dict
@@ -43,20 +40,34 @@ def about(request):
 
 
 def add_stock(request):
-    if request.method == "POST":
+    if request.method == "POST": # add to db
         form = StockForm(request.POST or None)
 
         if form.is_valid():
             form.save()
             messages.success(request, ("Stock Has Been Added."))
             return redirect(add_stock)
-    else:
+
+    else: # call db
         ticker = Stock.objects.all()
-        return render(request, "add_stock.html", {'ticker': ticker})
+
+        output = []
+        for symbol in ticker:
+            target_url = os.path.join(ENDPOINT, str(symbol))
+            response = requests.get(url=target_url, params=parameters)
+            response.raise_for_status()
+            output.append(response.json()[0])
+
+        return render(request, "add_stock.html", {'ticker': ticker, 'output': output})
 
 
 def delete(request, stock_id: int):
     item = Stock.objects.get(pk=stock_id)
     item.delete()
     messages.success(request, ("Stock Has Been Deleted!"))
-    return redirect(add_stock)
+    return redirect(delete_stock)
+
+
+def delete_stock(request):
+    ticker = Stock.objects.all()
+    return render(request, "delete_stock.html", {'ticker': ticker})
